08a66bd3e2995b1119a0b9575cf70d2cfed9ec2c
contygm <contygm@gmail.com>
2021-07-27 10:29:02 -0700

    ch2 file added todos

---
+++ tsCode/chapters/ch2.ts
@@ -0,0 +1,10 @@
+// TODO 2.1 Remove Dups
+
+
+// TODO 2.2 Return Kth to Last
+// TODO 2.3 Delete Middle Nose
+// TODO 2.4 Partition
+// TODO 2.5 Sum Lists
+// TODO 2.6 Palindrome
+// TODO 2.7 Intersection
+// TODO 2.8 Loop Detection
